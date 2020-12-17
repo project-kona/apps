730c479401554230ccb217e11464bf3906e07076
Irina Calciu <icalciu@vmware.com>
2020-12-17 15:59:36 -0800

    scripts

--- redis/setup.sh
+++ redis/setup.sh
@@ -1,35 +1,50 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-pushd ${SCRIPT_DIR}
+# install deps
+###########################
+install_deps() {
+  sudo apt-get install -y build-essential autoconf automake \
+        libpcre3-dev libevent-dev pkg-config \
+        zlib1g-dev tcl8.6 libssl-dev
+}
 
 # Redis
 ##########################
-git clone https://github.com/antirez/redis
-pushd redis 
+install_redis() {
+  git clone https://github.com/antirez/redis
+  pushd redis 
     make distclean # important! 
     make -j$(nproc)
 
     # Use already-provided Redis file
     cp ../redis-memory.conf ./redis.conf
-popd
+  popd
+}
 
-sudo sh -c "echo 'vm.overcommit_memory=1' >> /etc/sysctl.conf"
-sudo sysctl vm.overcommit_memory=1
+configure_redis() {
+  sudo sh -c "echo 'vm.overcommit_memory=1' >> /etc/sysctl.conf"
+  sudo sysctl vm.overcommit_memory=1
 
-sudo sh -c "echo never > /sys/kernel/mm/transparent_hugepage/enabled"
-sudo sh -c "echo 'echo never > /sys/kernel/mm/transparent_hugepage/enabled' >> /etc/rc.local"
+  sudo sh -c "echo never > /sys/kernel/mm/transparent_hugepage/enabled"
+  sudo sh -c "echo 'echo never > /sys/kernel/mm/transparent_hugepage/enabled' >> /etc/rc.local"
+  sudo sh -c "echo 1024 > /proc/sys/net/core/somaxcon"
+}
 
 
 # Memtier
 ##########################
-sudo apt-get install -y libpcre3-dev  libevent-dev
-
-git clone https://github.com/RedisLabs/memtier_benchmark
-pushd memtier_benchmark
+install_memtier() {
+  git clone https://github.com/RedisLabs/memtier_benchmark
+  pushd memtier_benchmark
     autoreconf -ivf
     ./configure
     make $(nproc)
     sudo make install
-popd
+  popd
+}
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pushd ${SCRIPT_DIR}
+install_redis
+configure_redis
+install_memtier
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,17 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 sudo apt-get update
 
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=36000'
+
+init_host() {
+  sudo apt-get update
+  sudo apt-get install libnuma-dev numactl build-essential  \
+                       python3 python3-pip python3-matplotlib \
+                       python3-numpy git wget libevent-dev 
+}
+
+init_host
 sudo python3 ${SCRIPT_DIR}/get-pip.py
 sudo python ${SCRIPT_DIR}/get-pip.py
 
