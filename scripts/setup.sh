1a7716d4a294c4295681885535549e7cfd3dd9ee
Irina Calciu <icalciu@vmware.com>
2020-12-17 21:13:30 -0500

    setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,14 +2,12 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sudo apt-get update
-
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=36000'
 
 init_host() {
   sudo apt-get update
-  sudo apt-get install libnuma-dev numactl build-essential  \
+  sudo apt-get -yq install libnuma-dev numactl build-essential  \
                        python3 python3-pip python3-matplotlib \
                        python3-numpy git wget libevent-dev 
 }
@@ -19,6 +17,6 @@ sudo python3 ${SCRIPT_DIR}/get-pip.py
 sudo python ${SCRIPT_DIR}/get-pip.py
 
 # Setup all applications
-${SCRIPT_DIR}/../apps/redis/setup.sh
-${SCRIPT_DIR}/../apps/metis/setup.sh
-${SCRIPT_DIR}/../apps/turi/setup.sh
+${SCRIPT_DIR}/../redis/setup.sh
+${SCRIPT_DIR}/../metis/setup.sh
+${SCRIPT_DIR}/../turi/setup.sh
